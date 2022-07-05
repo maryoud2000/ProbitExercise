fdcd192e5b63e8c5bd67daacf17bd78d79690930
maryoud2000 <maryoud2000@gmail.com>
2022-07-05 02:45:33 +0100

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProbitExercise.MvcApplication" Language="C#" %>
---
+++ Scripts/signIn.js
@@ -0,0 +1,8 @@
+﻿function loginCheck() {   
+    if (form.email.value == "maryoud2000@gmail.com" && form.password.value == "123") {
+        window.location.href = "/Home/Welcome";
+    }
+    else {
+        alert("Error Password or Username")
+    }
+}
